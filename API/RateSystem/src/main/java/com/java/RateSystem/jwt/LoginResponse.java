495fde5d6cbf32182d288a5fc3bc585637c95d2c
phamleanh05 <phamleanh05@gmail.com>
2022-06-09 17:11:26 +0700

    Export data inner join

---
+++ API/RateSystem/src/main/java/com/java/RateSystem/jwt/LoginResponse.java
@@ -0,0 +1,12 @@
+package com.java.RateSystem.jwt;
+
+import lombok.Data;
+
+@Data
+public class LoginResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
--- API/RateSystem/src/main/java/com/java/RateSystem/service/UserService.java
+++ API/RateSystem/src/main/java/com/java/RateSystem/service/UserService.java
@@ -1,10 +1,16 @@
 package com.java.RateSystem.service;
 
+import com.java.RateSystem.jwt.CustomUserDetails;
 import com.java.RateSystem.models.Servicerate;
 import com.java.RateSystem.models.User;
 import com.java.RateSystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,11 +18,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
 
+//    @Autowired
+//    PasswordEncoder passwordEncoder;
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
@@ -39,5 +47,24 @@ public class UserService {
         return userRepository.existsById(id);
     }
 
-    public User save(@RequestBody User newUser) { return userRepository.save(newUser);}
+    public User save(@RequestBody User newUser) {
+//        String s = newUser.getPassword();
+//        newUser.setPassword(passwordEncoder.encode(s));
+        return userRepository.save(newUser);
+    }
+
+    public UserDetails loadUserByUsername(String username) {
+        User user = userRepository.findByName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new CustomUserDetails(user);
+    }
+    @Transactional
+    public UserDetails loadUserById(Integer id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found with id : " + id)
+        );
+        return new CustomUserDetails(user);
+    }
 }
