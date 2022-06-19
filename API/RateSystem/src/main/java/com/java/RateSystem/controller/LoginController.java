2a612b89effb5538c72962763010d9f7ff49baf7
phamleanh05 <phamleanh05@gmail.com>
2022-06-19 14:55:46 +0700

    Final

--- API/RateSystem/src/main/java/com/java/RateSystem/controller/LoginController.java
+++ API/RateSystem/src/main/java/com/java/RateSystem/controller/LoginController.java
@@ -1,30 +1,30 @@
-package com.java.RateSystem.controller;
-
-import com.java.RateSystem.models.User;
-import com.java.RateSystem.service.LoginService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-
-@CrossOrigin(origins = "*", allowedHeaders = "", exposedHeaders = "*")
-@RestController
-public class LoginController {
-    @Autowired
-    private LoginService loginService;
-
-
-    @PostMapping("/login")
-    public String login(@RequestBody User user){
-        System.out.println("login request: " + user);
-        User authenticated = loginService.authenticate(user.getName(), user.getPassword());
-        if (authenticated != null){
-            return "Ok";
-        }
-        else {
-            return "Error";
-        }
-    }
-}
+//package com.java.RateSystem.controller;
+//
+//import com.java.RateSystem.models.User;
+//import com.java.RateSystem.service.LoginService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//
+//@CrossOrigin(origins = "*", allowedHeaders = "", exposedHeaders = "*")
+//@RestController
+//public class LoginController {
+//    @Autowired
+//    private LoginService loginService;
+//
+//
+//    @PostMapping("/login")
+//    public String login(@RequestBody User user){
+//        System.out.println("login request: " + user);
+//        User authenticated = loginService.authenticate(user.getName(), user.getPassword());
+//        if (authenticated != null){
+//            return "Ok";
+//        }
+//        else {
+//            return "Error";
+//        }
+//    }
+//}
--- API/RateSystem/src/main/java/com/java/RateSystem/controller/TestTokenController.java
+++ API/RateSystem/src/main/java/com/java/RateSystem/controller/TestTokenController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@CrossOrigin(origins = "*", allowedHeaders = "", exposedHeaders = "*")
 @RestController
 @RequestMapping("/api")
 public class TestTokenController {
